d8982ab2fe37febf0be6a4281378f535c0b52f18
eru1030 <476562694@qq.com>
2021-05-16 16:07:35 +0800

    Update 0main.py

--- 0main.py
+++ 0main.py
@@ -1,6 +1,6 @@
 import os
-os.system("python ./1爬取py3.py")
-os.system("python ./2时间变量筛选py38.py")
-os.system("python ./3输出选股ad.py")
+os.system("python ./1爬取tusharepro.py")
+os.system("python ./2计算指标后画出图形存图.py")
+
 
 
