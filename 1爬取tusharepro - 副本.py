a56ca0da7a22d398662528be36f8ee4229a7cd4e
eru1030 <476562694@qq.com>
2021-05-16 16:01:07 +0800

    Update 1爬取tusharepro - 副本.py

--- 1爬取tusharepro - 副本.py
+++ 1爬取tusharepro - 副本.py
@@ -10,7 +10,7 @@ token="c8811a29e9b51d03b2b3f6be3937f331d6187999563900664056c438"
 ts.set_token(token)
 pro=ts.pro_api()
 url = []
-df=pd.read_csv("d://1gddata/set/codelist_del_st.csv",encoding="unicode_escape")
+df=pd.read_csv("set/codelist_del_st.csv",encoding="unicode_escape")
 alist=df.code.tolist()
 for i in alist:
     x="%06d" % i
@@ -21,7 +21,7 @@ y=0
 while y<l:
     c=url[y]
     df1=pro.daily(ts_code=c,start_date='20170101',end_date=dateToday)
-    df1.to_csv("kdata/%s.csv"%c)
+    df1.to_csv("data/%s.csv"%c)
     y=y+1
     time.sleep(1)
     
